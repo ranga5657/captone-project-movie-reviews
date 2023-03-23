a8a0120a8763b1e5980d28c1cadc0694bfa89e8e
ranga5657 <ranga1695@gmail.com>
2023-03-23 12:47:11 +0530

    movie-login

---
+++ movie-comment/src/test/java/org/movie/comments/MovieCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.movie.comments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

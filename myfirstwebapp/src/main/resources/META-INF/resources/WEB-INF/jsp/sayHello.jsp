9f2a6c5f79f319e42b1400bb564b464a507727ed
niranjan0326 <niranjanreddy242@gmail.com>
2023-03-23 12:36:13 +0530

    a

---
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>My first HTML page - JSP</title>
+    </head>
+    <body>
+        <h1>Heading</h1>
+        <h1>Heading 2</h1>
+
+        <h1>first html page with body - JSP</h1> 
+    </body>
+</html>
---
+++ myfirstwebapp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10001,'niranjan','Get AWS Certified',CURRENT_DATE(),false);
+
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10002,'niranjan','Learn Cloud Computing',CURRENT_DATE(),false);
+
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10003,'niranjan','Learn DevOps',CURRENT_DATE(),false);
+
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10004,'niranjan','Learn Java Full Stack',CURRENT_DATE(),false);
